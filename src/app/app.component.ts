a91882dd9580ffc2df952765db250635496842dd
Roger Terrill <lpaddikt@gmail.com>
2022-03-28 22:23:51 -0700

    Date pipes and remember predefined types

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,8 +6,10 @@ import {Component} from "@angular/core";
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  name = 'luis ramirez'
-  imgUrl = 'https://picsum.photos/id/237/500/500'
+  name = 'luis ramirez';
+  imgUrl = 'https://picsum.photos/id/237/500/500';
+  title = 'basics';
+  currentDate = new Date();
 
   getName() {
     return this.name;
